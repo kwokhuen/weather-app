768bf505f42b41a755213c7bcfa509a54d9403fd
kwokhuen <benjaminkwokhuen@gmail.com>
2017-01-21 14:46:37 -0800

    implementing google geocode api to fetch location

---
+++ geocode/geocode.js
@@ -0,0 +1,22 @@
+const request = require('request');
+
+module.exports = {
+  geocodeAddress: (address, callback) => {
+    request({
+      url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}`,
+      json: true
+    }, (error, response, body) => {
+      if (error) {
+        callback('Unable to connect to google server');
+      } else if (body.status === 'ZERO_RESULTS'){
+        callback('Unable to find that address');
+      } else if (body.status === 'OK') {
+        callback(undefined, {
+          address: body.results[0].formatted_address,
+          latitude: body.results[0].geometry.location.lat,
+          longitude: body.results[0].geometry.location.lng
+        });
+      }
+    });
+  }
+}
